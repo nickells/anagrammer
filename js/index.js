3f16fc68656bd8e0952ce4a6d8d73c6559fde650
nickells <ellsworth.nick@gmail.com>
2017-04-14 17:30:18 -0400

    refactor offsets logic

--- js/index.js
+++ js/index.js
@@ -48,30 +48,31 @@ function getOffsets(domArray) {
   return map
 }
 
-function getDifferenceOfOffsets(toObject, fromObject) {
-  return Object.keys(toObject).reduce((newObject, key) => {
-    // set up new data structure
-    if (!newObject[key]) {
-      newObject[key] = {
-        $elems: [],
-        offsets: []
-      }
-    }
-    toObject[key].$elems.forEach((elem, index) => {
-      newObject[key].$elems[index] = fromObject[key].$elems[index]
-    })
-    toObject[key].offsets.forEach((offset, index) => {
-      newObject[key].offsets[index] = fromObject[key].offsets[index] - offset
-    })
-    return newObject
-  }, {})
+const sortDomElements = (elem, elem2) => {
+  if (elem.innerHTML < elem2.innerHTML) return -1
+  else if (elem.innerHTML > elem2.innerHTML) return 1
+  else return 0
 }
 
-function anagramize(from, to, selector) {
+function getDifferenceOfOffsets(toText, fromText) {
+  let sortedToText = toText.sort(sortDomElements)
+  let sortedFromText = fromText.sort(sortDomElements)
+
+  return sortedFromText.map((elem, index) => {
+    return {
+      elem,
+      distance: sortedToText[index].getBoundingClientRect().left - elem.getBoundingClientRect().left
+    }
+  })
+}
+
+function anagramize(to, from, selector) {
   const container = document.getElementById(selector)
+
   const fromText = document.createElement('h1')
   fromText.innerHTML = spanIze(from)
   container.appendChild(fromText)
+
   const toText = document.createElement('h1')
   toText.innerHTML = spanIze(to)
   container.appendChild(toText)
@@ -79,18 +80,13 @@ function anagramize(from, to, selector) {
   const toTextChildren = Array.from(toText.children)
   const fromTextChildren = Array.from(fromText.children)
 
-  const toDistances = getOffsets(toTextChildren)
-  const fromDistances = getOffsets(fromTextChildren)
+  let toMove = getDifferenceOfOffsets(toTextChildren, fromTextChildren)
 
-  let toMove = getDifferenceOfOffsets(toDistances, fromDistances)
-
-  const shuffledFromTextChildren = RANDOMIZE ? shuffle(fromTextChildren) : fromTextChildren
+  const shuffledFromTextChildren = RANDOMIZE ? shuffle(toMove) : toMove
 
   function iterateAnimating(index){
-    if (index === shuffledFromTextChildren.length) return
-    let letter = shuffledFromTextChildren[index].innerText
-    const distance = toMove[letter].offsets.shift()
-    const span  = toMove[letter].$elems.shift()
+    if (index === toMove.length) return
+    let {elem: span, distance} = toMove[index]
     if (distance !== 0){ 
       requestAnimationFrame(() => {
         const directionClass = Math.random() > 0.5 ? 'is-background' : 'is-foreground'
@@ -102,13 +98,13 @@ function anagramize(from, to, selector) {
         
         // begin animation
         setTimeout(()=>{
-          span.style.transform = `translateX(${-distance}px) scale(${scale})`
+          span.style.transform = `translateX(${distance}px) scale(${scale})`
         }, TRANSITION_DELAY)
 
         // move back to initial z distance
         setTimeout(()=>{
           span.classList.remove(directionClass)
-          span.style.transform = `translateX(${-distance}px) scale(1.0)`
+          span.style.transform = `translateX(${distance}px) scale(1.0)`
         }, TRANSITION_TIME + TRANSITION_DELAY)
       })
     }
